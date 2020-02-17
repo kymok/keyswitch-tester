3e0b96d94f00bbaaf04b9987080778707093d8e0
kymok <kaoru.yamaoka@gmail.com>
2020-02-17 11:31:14 +0900

    Updates

---
+++ precache-manifest.5c08db7faf6137d7e111485d3f4a8899.js
@@ -0,0 +1,226 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b8469b896b3f40e6b87cdb94bc72de5",
+    "url": "/keyswitch-tester/index.html"
+  },
+  {
+    "revision": "5291acebf68db1312564",
+    "url": "/keyswitch-tester/static/css/main.42c69e1e.chunk.css"
+  },
+  {
+    "revision": "c7deb2517198278a5a88",
+    "url": "/keyswitch-tester/static/js/2.c7b40770.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/keyswitch-tester/static/js/2.c7b40770.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5291acebf68db1312564",
+    "url": "/keyswitch-tester/static/js/main.c652a9cf.chunk.js"
+  },
+  {
+    "revision": "cf07ff55a33b6466aa25",
+    "url": "/keyswitch-tester/static/js/runtime-main.66a0a839.js"
+  },
+  {
+    "revision": "dfa9d945247c51d1d130e29f7ff3658e",
+    "url": "/keyswitch-tester/static/media/aliaz_silent.dfa9d945.jpg"
+  },
+  {
+    "revision": "28773d4d9764988b2fd4a1f7e5ae14f6",
+    "url": "/keyswitch-tester/static/media/aqua_zilent_62.28773d4d.jpg"
+  },
+  {
+    "revision": "2bc194a164e00825649be70b7da98ca5",
+    "url": "/keyswitch-tester/static/media/aqua_zilent_67.2bc194a1.jpg"
+  },
+  {
+    "revision": "cbf15d80eefb28325a4ae35f37c42cc8",
+    "url": "/keyswitch-tester/static/media/cherry_mx_blue.cbf15d80.jpg"
+  },
+  {
+    "revision": "8a079999801ae0badf9be025ebbbdd43",
+    "url": "/keyswitch-tester/static/media/cherry_mx_brown.8a079999.jpg"
+  },
+  {
+    "revision": "7ed55ce3bba7ff328ad98a9a3ed6750b",
+    "url": "/keyswitch-tester/static/media/cherry_mx_red.7ed55ce3.jpg"
+  },
+  {
+    "revision": "f053baa5b612907d4f132340b5781f46",
+    "url": "/keyswitch-tester/static/media/cherry_mx_rgb_silent_pink.f053baa5.jpg"
+  },
+  {
+    "revision": "2127d4f1d273a2e4910d02a7a9339873",
+    "url": "/keyswitch-tester/static/media/cherry_mx_silver.2127d4f1.jpg"
+  },
+  {
+    "revision": "127e063257d3a96641deba5792f97d36",
+    "url": "/keyswitch-tester/static/media/close.127e0632.svg"
+  },
+  {
+    "revision": "9ac33522c2b3028696048a82daa08808",
+    "url": "/keyswitch-tester/static/media/gateron_black.9ac33522.jpg"
+  },
+  {
+    "revision": "387e49722003e88c7cbd339133ae2330",
+    "url": "/keyswitch-tester/static/media/gateron_ink_black.387e4972.jpg"
+  },
+  {
+    "revision": "f00e23181d23790f0764fdfb6d6067c0",
+    "url": "/keyswitch-tester/static/media/gateron_ink_blue.f00e2318.jpg"
+  },
+  {
+    "revision": "f9b683d95e6b4e4affa343b505ca7541",
+    "url": "/keyswitch-tester/static/media/gateron_ink_red.f9b683d9.jpg"
+  },
+  {
+    "revision": "3f4f38c08bd6064de6821c0dd3804fce",
+    "url": "/keyswitch-tester/static/media/gateron_ink_yellow.3f4f38c0.jpg"
+  },
+  {
+    "revision": "b3b00da9d29f8bfe383d5d9a5bead47d",
+    "url": "/keyswitch-tester/static/media/gateron_milk_milk_black.b3b00da9.jpg"
+  },
+  {
+    "revision": "dc36bacb38f308080e834dd75ad96f97",
+    "url": "/keyswitch-tester/static/media/gateron_silent_black.dc36bacb.jpg"
+  },
+  {
+    "revision": "cd5cf5a9aa8ce463c393472b1acbdc00",
+    "url": "/keyswitch-tester/static/media/gateron_silent_brown.cd5cf5a9.jpg"
+  },
+  {
+    "revision": "c8cee5d3a5ffa520de9e77ffdd4e0fff",
+    "url": "/keyswitch-tester/static/media/gateron_silent_red.c8cee5d3.jpg"
+  },
+  {
+    "revision": "ed9c67c8eae5a2fbf149d3748f79915c",
+    "url": "/keyswitch-tester/static/media/gateron_yellow.ed9c67c8.jpg"
+  },
+  {
+    "revision": "51f9bfc578e2b17e67bc65d9f401a06f",
+    "url": "/keyswitch-tester/static/media/hako_true.51f9bfc5.jpg"
+  },
+  {
+    "revision": "48bbe92c4e6ed02b7d549a3c76347a7d",
+    "url": "/keyswitch-tester/static/media/kailh_box_black.48bbe92c.jpg"
+  },
+  {
+    "revision": "f5a2f523b17965613ab19982b080fb42",
+    "url": "/keyswitch-tester/static/media/kailh_box_brown.f5a2f523.jpg"
+  },
+  {
+    "revision": "19ea6585084696b476bafcb172ceacc9",
+    "url": "/keyswitch-tester/static/media/kailh_box_red.19ea6585.jpg"
+  },
+  {
+    "revision": "d5ae3b9bf9de967350a827d2c88262c2",
+    "url": "/keyswitch-tester/static/media/kailh_box_white.d5ae3b9b.jpg"
+  },
+  {
+    "revision": "3464b612d2b5e2ee9de2e9b54966ba32",
+    "url": "/keyswitch-tester/static/media/kailh_pro_burgundy.3464b612.jpg"
+  },
+  {
+    "revision": "bf78ac325e8c34ff8fd902b9d3a56ca4",
+    "url": "/keyswitch-tester/static/media/kailh_pro_light_green.bf78ac32.jpg"
+  },
+  {
+    "revision": "dbcff79ef698e3a3194739ebef763357",
+    "url": "/keyswitch-tester/static/media/kailh_pro_purple.dbcff79e.jpg"
+  },
+  {
+    "revision": "f2206917ec8f62e91ff7e4dd6150620b",
+    "url": "/keyswitch-tester/static/media/kailh_speed_copper.f2206917.jpg"
+  },
+  {
+    "revision": "6e3ee9b54db1ed99d18dd064c629518a",
+    "url": "/keyswitch-tester/static/media/kailh_speed_gold.6e3ee9b5.jpg"
+  },
+  {
+    "revision": "736ba066fa58e0842ab8b7e3a9c69cfe",
+    "url": "/keyswitch-tester/static/media/kailh_speed_pink.736ba066.jpg"
+  },
+  {
+    "revision": "f4fbf48498c77a12cf2349bcdf32ac65",
+    "url": "/keyswitch-tester/static/media/kailh_speed_silver.f4fbf484.jpg"
+  },
+  {
+    "revision": "39727d4114f6058d132c7e8790e77cae",
+    "url": "/keyswitch-tester/static/media/no_image.39727d41.svg"
+  },
+  {
+    "revision": "797a3010cb90161013ce15db5b715f88",
+    "url": "/keyswitch-tester/static/media/novelkeys_box_burnt_orange.797a3010.jpg"
+  },
+  {
+    "revision": "ae3c1d1d7890f84c54732adbc15292cd",
+    "url": "/keyswitch-tester/static/media/novelkeys_box_dark_yellow.ae3c1d1d.jpg"
+  },
+  {
+    "revision": "725210c76df6f0845d53c0492aa414ef",
+    "url": "/keyswitch-tester/static/media/novelkeys_box_pale_blue.725210c7.jpg"
+  },
+  {
+    "revision": "72ed6b6703b495523d34b74f6c0f2281",
+    "url": "/keyswitch-tester/static/media/novelkeys_box_royal.72ed6b67.jpg"
+  },
+  {
+    "revision": "3259c28b6cb1ecb32fd17496c392e1ae",
+    "url": "/keyswitch-tester/static/media/novelkeys_cream.3259c28b.jpg"
+  },
+  {
+    "revision": "9a7f6edd8a5b86957d54aa1e1be31539",
+    "url": "/keyswitch-tester/static/media/roselios.9a7f6edd.jpg"
+  },
+  {
+    "revision": "a38f346e6a069395fc4c6da78606c246",
+    "url": "/keyswitch-tester/static/media/sakurios.a38f346e.jpg"
+  },
+  {
+    "revision": "6fe11d00fbc8b7695151cee9cf99feb7",
+    "url": "/keyswitch-tester/static/media/tealios.6fe11d00.jpg"
+  },
+  {
+    "revision": "b77a2f947330c8d5a2339758293f3a97",
+    "url": "/keyswitch-tester/static/media/turquoise_tealios_63_5.b77a2f94.jpg"
+  },
+  {
+    "revision": "13b75a348ca2212f21e1c97c8cc5de96",
+    "url": "/keyswitch-tester/static/media/turquoise_tealios_65.13b75a34.jpg"
+  },
+  {
+    "revision": "3b75fed4610afb01ad48d179d8d77a5d",
+    "url": "/keyswitch-tester/static/media/zealios_v2_62.3b75fed4.jpg"
+  },
+  {
+    "revision": "b7ef7ddf58cadb7b0d3b27e14f901e7f",
+    "url": "/keyswitch-tester/static/media/zealios_v2_65.b7ef7ddf.jpg"
+  },
+  {
+    "revision": "4dd2a614de97be2e3230d161e8df2c25",
+    "url": "/keyswitch-tester/static/media/zealios_v2_67.4dd2a614.jpg"
+  },
+  {
+    "revision": "909cae33c61a5379323cd53924db1a5d",
+    "url": "/keyswitch-tester/static/media/zealios_v2_78.909cae33.jpg"
+  },
+  {
+    "revision": "4971de5b6f8d5bd7f4988ab0ab452341",
+    "url": "/keyswitch-tester/static/media/zilent_v2_62.4971de5b.jpg"
+  },
+  {
+    "revision": "06347ce055907942b83ab38b28cac219",
+    "url": "/keyswitch-tester/static/media/zilent_v2_65.06347ce0.jpg"
+  },
+  {
+    "revision": "d59b5daaf17f42afef7b845b651956ce",
+    "url": "/keyswitch-tester/static/media/zilent_v2_67.d59b5daa.jpg"
+  },
+  {
+    "revision": "e69385d7ad5b34ef894364ce2cee7d9c",
+    "url": "/keyswitch-tester/static/media/zilent_v2_78.e69385d7.jpg"
+  }
+]);
